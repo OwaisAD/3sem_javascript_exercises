5328de9e2f04407c94031104cc9f02ac1231d2f1
Owais Dashti <owaisdashti@outlook.com>
2022-09-19 14:49:30 +0200

    exercise 1 done

---
+++ JS_exercises_day1/exercises.js
@@ -0,0 +1,31 @@
+// EXERCISE 1
+
+// A1) Data types in JS are strings, numbers, booleans, undefined and null. We refer to them as primitive types.
+
+// A2) Undefined variable means a variable has been declared but it does not have a value. Undeclared variable means that the variable does not exist in the program at all.
+
+// A3) Single line is //, multiline /**/, function comment /**... */
+
+// A4) Does JavaScript support automatic type conversion? Yes. It's usually called type coercion, but conversion is perfectly accurate.
+
+// A5) https://www.w3schools.com/js/js_strict.asp Strict mode eliminates some JavaScript silent errors by changing them to throw errors. Strict mode fixes mistakes that make it difficult for JavaScript engines to perform optimizations: strict mode code can sometimes be made to run faster than identical code that's not strict mode. You use it by writing "use strict"
+
+
+// EXERCISE 2
+
+
+
+// EXERCISE 3
+
+
+// EXERCISE 4
+
+
+// EXERCISE 5
+
+
+// EXERCISE 6
+
+
+// EXERCISE 7
+
