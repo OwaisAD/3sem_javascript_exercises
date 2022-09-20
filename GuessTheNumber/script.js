d200f8c863ade3b579d5a350164e339380a607bc
Owais Dashti <owaisdashti@outlook.com>
2022-09-20 15:09:41 +0200

    guess my number game is working

--- GuessTheNumber/script.js
+++ GuessTheNumber/script.js
@@ -6,13 +6,15 @@ const guessButton = document.querySelector("button.check") //could select it mul
 const restartButton = document.querySelector("button.again")
 const message = document.querySelector(".message")
 const displayResult = document.querySelector(".number")
-//const score = document.querySelector("span.score")
+const score = document.querySelector("span.score")
+const highScore = document.querySelector("span.highscore")
 const tries = document.querySelector("span.tries")
 const header = document.querySelector("h1.header")
 
 //Helper functions
 // amount of numbers
 let n = 20
+let lost = false //deciding when it's a highscore
 
 const getRandomNumber = (n) => {
     return Math.ceil(Math.random() * n)
@@ -23,6 +25,7 @@ const checkIfLost = ()  => {
         guessButton.disabled = true
         header.innerText = "You lost..."
         message.innerText = "TOO BAD GO AGAIN!"
+        lost = true
     }
 }
 
@@ -32,6 +35,11 @@ const evaluateGuess = () => {
             message.innerText = "You hit the number 🎆!"
             displayResult.innerText = randomNumber;
             header.innerText = "YOU WON!!"
+            guessButton.disabled = true
+            score.innerText = Number(score.innerText) + 20
+            if(lost == false && score.innerText > highScore.innerText) {
+                highScore.innerText = score.innerText
+            }
             break;
         case guessInputBox.value > randomNumber:
             message.innerText = "Try lower ⬇️ mate.."
@@ -61,6 +69,14 @@ restartButton.addEventListener("click", () => {
     message.innerText = "YOUR TIME TO SHINE, GUESS!!"
     tries.innerText = 5
     guessButton.disabled = false
+
+    // check if the last round was a win or not
+    // if round was lost then reset 0 otherwise keep going
+    if(lost) {
+        score.innerText = 0
+        lost = false
+    }
+    
     randomNumber = getRandomNumber(n)
     console.log("Restarted new number: ", randomNumber)
 })
