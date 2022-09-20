8cd6b0370403fb1962b17ba7699ec696192d1d6b
Owais Dashti <owaisdashti@outlook.com>
2022-09-20 20:03:14 +0200

    exercise 2 done

--- dom_manipulation/exercise2/script.js
+++ dom_manipulation/exercise2/script.js
@@ -1,4 +1,10 @@
-document.querySelectorAll("div").forEach(
+/*document.querySelectorAll("div").forEach(
     (el) => el.addEventListener("click", () => {
-    console.log(`Hi from ${el.idg}`)
-}))
+    console.log(`Hi from ${el.id}`)
+}))*/
+
+const paragraph = document.getElementById("para")
+document.querySelector("div#outer").addEventListener("click", (e) => {
+    //console.log(`${e.target.id}`)
+    para.innerText += ` ${e.target.id}`
+})
