9b17021573fa1b3d18a76776344a76875bf90abb
Owais Dashti <owaisdashti@outlook.com>
2022-09-20 21:22:35 +0200

    fixed name exercise 3

---
+++ dom_manipulation/exercise3and4/script.js
@@ -0,0 +1,31 @@
+const submitBtn = document.querySelector("form button")
+const myNamesList = document.getElementById("list")
+var names = ["Lars", "Peter", "Jan", "Bo", "Frederick"]
+
+const createUlList = (namesList) => {
+    var liNames = names.map(name => `<li>${name}</li>`)
+    var myUlList = `<ul>${"\n"}${liNames.join("\n    ")}${"\n"}</ul>`
+    console.log(myUlList)
+    myNamesList.innerHTML = myUlList
+}
+
+createUlList(names)
+
+const addName = (event) => {
+    event.preventDefault()
+    var name = document.getElementById("nameInput").value;
+    names.push(name)
+    createUlList()
+}
+
+const removeFirst = (event) => {
+    event.preventDefault()
+    names.shift()
+    createUlList(names)
+}
+
+const removeLast = (event) => {
+    event.preventDefault()
+    names.pop()
+    createUlList(names)
+}
