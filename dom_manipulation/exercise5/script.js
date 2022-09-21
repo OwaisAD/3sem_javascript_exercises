972db24e655fbaa1bf06413d7faecee8909c5b88
Owais Dashti <owaisdashti@outlook.com>
2022-09-21 17:41:40 +0200

    calculator done

--- dom_manipulation/exercise5/script.js
+++ dom_manipulation/exercise5/script.js
@@ -1,14 +1,44 @@
-const buttons = document.querySelector("div#buttons")
+'use strict'
+
+const buttons = document.getElementById("buttons")
 const display = document.getElementById("display")
-const calculateButton = document.getElementById("calculate")
+const clearBtn = document.getElementById("clear")
+
+let number
+let operator
+
+const numbersList = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const operatorsList = ["+", "-", "/", "*"];
 
 buttons.addEventListener("click", (e) => {
-    if(e.target.innerText === "=") return
-    //display.innerText += e.target.innerText
-    console.log(e.target.innerText)
-})
+    console.log("Hit the following: ", e.target.textContent)
+    if (numbersList.includes(e.target.textContent)) {
+        display.innerHTML += e.target.textContent
+    } else if (operatorsList.includes(e.target.textContent)) {
+        number = parseFloat(display.innerText)
+        operator = e.target.textContent
+        console.log("Current number", number)
+        console.log("Current operator", operator)
+        display.innerHTML = ""
+    } else if (e.target.textContent === '.') {
+            display.innerHTML += e.target.textContent
+    } else if (e.target.textContent === '=') {
+        let result;
+        if (operator === "+") {
+            result = number + parseFloat(display.textContent);
+        } else if (operator === "-") {
+            result = number - parseFloat(display.textContent);
+        } else if (operator === "/") {
+            result = number / parseFloat(display.textContent);
+        } else if (operator === "*") {
+            result = number * parseFloat(display.textContent);
+        }
+        // get result and show it in the display
+        display.innerHTML = result
+    }
+});
 
-calculateButton.addEventListener("click", (event) => {
-    event.stopPropagation() //preventing this event from bubbling up to our "outer" event handler
-    
+clearBtn.addEventListener("click", () => {
+    //quick hack
+    window.location.reload()
 })
